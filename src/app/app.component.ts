0bf04b2bdf02bad55c7936b7bb635fe591768831
Chase <arinchase@hotmail.com>
2022-03-24 14:48:48 -0500

    Fixed scrolling issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router, RouterEvent } from '@angular/router';
+import { filter, pairwise, startWith, Subject, takeUntil } from 'rxjs';
 
-// Component Decorator
-//  - Selector: Defines HTML element that can be inbedded
-//  - templateUrl: Html component for module
-//  - styleUrls: Styling component for module
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'PortfolioProject';
+export class AppComponent implements AfterViewInit, OnDestroy {
+
+  title = 'PortfolioProject'
+
+  public destroyed = new Subject<any>();
+
+  @ViewChild('content', { static: false }) content!: ElementRef<HTMLDivElement>
+
+  constructor(private router: Router) { }
+
+  ngAfterViewInit(): void {
+
+    /* Scroll to top of content page on navigation, smooth scroll if same url navigation */
+    this.router.events.pipe(
+      filter((event: any) => event instanceof NavigationEnd),
+      pairwise(),
+      startWith('Initial call'),
+      takeUntil(this.destroyed)
+    ).subscribe((events: any) => { 
+        if(events[0].url === events[1].url) { // Same page navigation
+          this.content.nativeElement.querySelector('.scroll')!.scrollIntoView({ 
+            behavior: 'smooth' 
+          })
+        }
+        else { // New Page
+          this.content.nativeElement.scrollTop = 0;
+        }
+    });
+
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed.next(true);
+    this.destroyed.complete();
+  }
+
 }
